57ffc6ae70710c564053fbf82ec998c80ff014fb
Austin Kramer <64988723+AKKramer@users.noreply.github.com>
2020-07-05 16:11:14 -0500

    Initial upload
    Landing page and button in work

---
+++ script.js
@@ -0,0 +1,14 @@
+// console.log('Im working')
+
+// $('#learnMoreBtn').click(
+//     console.log('Im working'),
+// )
+
+// $('#learnMoreBtn').hover(
+//     function(){
+//     var element = $('#learnMoreBtn')
+//     console.log('Im working'),
+//     // element.classList.toggle("fa fa-arrow-right")
+//     $(this).find('i').toggleClass('fa fa-arrow-down fa fa-arrow-right')
+// }
+// )
